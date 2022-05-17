83afc1dcb19508cc47c1f7ff8ce6f89e95715eba
Takayoshi Makabe <makabe.takayoshi1117@gmail.com>
2022-05-17 17:25:59 +0900

    Initial Commit

---
+++ HowToUsePhotoControllers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowToUsePhotoControllers
+//
+//  Created by 真壁孝嘉 on 2022/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HowToUsePhotoControllers/HowToUsePhotoControllersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUsePhotoControllersApp.swift
+//  HowToUsePhotoControllers
+//
+//  Created by 真壁孝嘉 on 2022/05/17.
+//
+
+import SwiftUI
+
+@main
+struct HowToUsePhotoControllersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
